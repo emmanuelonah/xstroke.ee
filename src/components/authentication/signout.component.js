bf77837af2be270131ae5fccc424c2fcdd02a1bf
Emmanuel Onah <emmajerry2016@gmail.com>
2020-09-20 21:12:49 +0300

    fixed logout error

--- src/components/authentication/signout.component.js
+++ src/components/authentication/signout.component.js
@@ -1,13 +1,15 @@
 window.addEventListener("DOMContentLoaded", _ => {
-  firebase
-    .auth()
-    .signOut()
-    .then(function () {
-      // Sign-out successful.
-      window.location.replace("/login");
-    })
-    .catch(function (error) {
-      // An error happened.
-      window.alert("Error Occured", error);
-    });
+  document.querySelector(".logout").addEventListener("click", _ => {
+    firebase
+      .auth()
+      .signOut()
+      .then(function () {
+        // Sign-out successful.
+        window.location.replace("/login");
+      })
+      .catch(function (error) {
+        // An error happened.
+        window.alert("Error Occured", error);
+      });
+  });
 });
