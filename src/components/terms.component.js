34005aef6a4a255d3dfcb9d705645bccf9061dd3
Emmanuel Onah <emmajerry2016@gmail.com>
2020-09-22 00:21:06 +0300

    configured terms functionality

--- src/components/terms.component.js
+++ src/components/terms.component.js
@@ -1,1 +1,14 @@
-window.addEventListener("DOMContentLoaded", _ => {});
+window.addEventListener("DOMContentLoaded", _ => {
+  const acceptanceButton = document.querySelector(".acceptance__button");
+  const declineButton = document.querySelector(".decline__button");
+
+  ///Accept Terms
+  acceptanceButton.addEventListener("click", _ => {
+    window.localStorage.setItem("acceptTerms", true);
+  });
+
+  /// Decline Terms
+  declineButton.addEventListener("click", _ => {
+    window.localStorage.setItem("acceptTerms", false);
+  });
+});
