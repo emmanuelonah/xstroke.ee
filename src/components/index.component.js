74b71c06a498a38632e9a7eaa2ce06a4d964d3a6
Emmanuel Onah <emmajerry2016@gmail.com>
2020-10-22 00:44:45 +0300

    redesigned landing page

--- src/components/index.component.js
+++ src/components/index.component.js
@@ -1,14 +1,15 @@
-window.addEventListener("DOMContentLoaded", _ => {
-  document.querySelector(".give__try").addEventListener("click", e => {
-    const _acceptTerms = window.localStorage.getItem("acceptTerms");
-    const _userLoggedInStatus = window.localStorage.getItem("userLoggedIn");
+window.addEventListener("DOMContentLoaded", (_) => {
+    document.querySelector("#proceed").addEventListener("click", () => {
+        const _acceptTerms = window.localStorage.getItem("acceptTerms");
+        const _userLoggedInStatus = window.localStorage.getItem("userLoggedIn");
 
-    if (_acceptTerms === "true" && (_userLoggedInStatus === "false") | undefined | _userLoggedInStatus) {
-      window.location.replace("/login");
-    } else if (_acceptTerms === "true" && _userLoggedInStatus === "true") {
-      window.location.replace("/user-profile");
-    } else {
-      window.location.replace("/terms");
-    }
-  });
+        console.log(_acceptTerms);
+        if (_acceptTerms === "true" && (_userLoggedInStatus === "false") | undefined) {
+            window.location.replace("/login");
+        } else if (_acceptTerms === "true" && _userLoggedInStatus === "true") {
+            window.location.replace("/user-profile");
+        } else {
+            window.location.replace("/terms");
+        }
+    });
 });
