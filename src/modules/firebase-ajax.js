87f86cd5926beb86c509d519fee596b6a8309c72
Emmanuel Onah <emmajerry2016@gmail.com>
2020-10-23 02:18:52 +0300

    updated ajax helper

--- src/modules/firebase-ajax.js
+++ src/modules/firebase-ajax.js
@@ -1,23 +1,25 @@
 const firestore = {
-  get(collection = "") {
-    const getOperation = db.collection(collection).onSnapshot(snapshot => {
-      snapshot.onChanges.map(change => {});
-    });
-    return; //
-  },
+    get(collection = "") {
+        const getOperation = db.collection(collection).onSnapshot((snapshot) => {
+            snapshot.onChanges.map((change) => {
+                return change;
+            });
+        });
+        return getOperation;
+    },
 
-  post(collection = "", body = {}) {
-    const postOperation = db.collection(collection).add(body);
-    return postOperation;
-  },
+    post(collection = "", body = {}) {
+        const postOperation = db.collection(collection).add(body);
+        return postOperation;
+    },
 
-  put(collection = "", docId = "") {
-    const putOperation = db.collection(collection).doc(docId).update();
-    return putOperation;
-  },
+    put(collection = "", docId = "") {
+        const putOperation = db.collection(collection).doc(docId).update();
+        return putOperation;
+    },
 
-  delete(collection = "", docId = "") {
-    const deleteOperation = db.collection(collection).doc(docId).delete();
-    return deleteOperation; //
-  },
+    delete(collection = "", docId = "") {
+        const deleteOperation = db.collection(collection).doc(docId).delete();
+        return deleteOperation;
+    },
 };
