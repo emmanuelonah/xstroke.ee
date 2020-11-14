d43352583002dea77b0d12399cd563bcde61ae17
Emmanuel Onah <emmajerry2016@gmail.com>
2020-11-14 17:43:34 +0200

    instant result done

--- src/components/diagnosis.component.js
+++ src/components/diagnosis.component.js
@@ -19,22 +19,26 @@ const diagnoseResult = (cigarettesDiagnose, physicalActivityDiagnose, bodyMassIn
     let massIndexResult;
     let bloodPressureResult;
 
-    let cigarretesMessage = `Decrease you cigarrete consumption to ${cigarreteResult}`;
+    let cigarretesMessage = `Decrease your cigarrete consumption to ${cigarreteResult}`;
     let physicalActivityMessage = `Increase your physical activity to ${physicalActivityResult}`;
     let massIndexMessage = `Decrease your body mass index to reach the target of ${massIndexResult}`;
     let bloodPressureMessage = `Decrease your blood pressure using agents prescribed by a doctor`;
 
     //check the cigarettesDianose and update the cigarreteResult
-    switch (true) {
-        case cigarettesDiagnose > 24:
+    switch (cigarettesDiagnose) {
+        case cigarettesDiagnose === 4:
             cigarreteResult = "13 - 12";
             break;
 
-        case cigarettesDiagnose === 13 && cigarettesDiagnose <= 24:
+        case cigarettesDiagnose === 3:
             cigarreteResult = "7 - 6";
             break;
 
-        case cigarettesDiagnose === 7 && cigarettesDiagnose <= 12:
+        case cigarettesDiagnose === 2:
+            cigarreteResult = "6 - 0";
+            break;
+
+        case cigarettesDiagnose === 1:
             cigarreteResult = "1 - 0";
             break;
 
@@ -43,25 +47,25 @@ const diagnoseResult = (cigarettesDiagnose, physicalActivityDiagnose, bodyMassIn
             break;
 
         default:
-            cigarreteResult = "13 - 12";
+            cigarretesMessage = "Please keep up the great work of zero cigarretes";
     }
 
     //check the physicalActivitiesDiagnose and update the physicalActivityResult
-    switch (true) {
+    switch (physicalActivityDiagnose) {
         case physicalActivityDiagnose === 4:
-            physicalActivityDiagnose = "1hr - 2hr:40mins";
+            physicalActivityResult = "1hr - 2hr:40mins";
             break;
 
         case physicalActivityDiagnose === 3:
-            physicalActivityDiagnose = "1hr:40mins - 2hrs:40mins";
+            physicalActivityResult = "1hr:40mins - 2hrs:40mins";
             break;
 
         case physicalActivityDiagnose === 2:
-            physicalActivityMessage = "2hrs:40mins - 3hrs:50mins";
+            physicalActivityResult = "2hrs:40mins - 3hrs:50mins";
             break;
 
         case physicalActivityDiagnose === 1:
-            physicalActivityDiagnose = "3hrs:50mins - if possible more hours";
+            physicalActivityResult = "3hrs:50mins - if possible more hours";
             break;
 
         case physicalActivityDiagnose === 0:
@@ -69,28 +73,28 @@ const diagnoseResult = (cigarettesDiagnose, physicalActivityDiagnose, bodyMassIn
             break;
 
         default:
-            physicalActivityMessage = "Keep up the great work while you continue the regular diagnosis";
+            physicalActivityResult = "1hr - 2hr:40mins";
     }
 
     // check the bodyMassIndexDiagnose and update the massIndexMessage
-    switch (true) {
-        case bodyMassIndexDiagnose >= 35:
+    switch (bodyMassIndexDiagnose) {
+        case bodyMassIndexDiagnose === 3:
             massIndexResult = "30 - 29.99";
             break;
 
-        case bodyMassIndexDiagnose === 30 && bloodPressureDiagnose <= 34.99:
+        case bodyMassIndexDiagnose === 2:
             massIndexResult = "25 - 24.99";
             break;
 
-        case bodyMassIndexDiagnose === 25 && bloodPressureDiagnose <= 29.99:
+        case bodyMassIndexDiagnose === 1:
             massIndexResult = "18.50";
             break;
 
-        case bodyMassIndexDiagnose === 18.5 && bloodPressureDiagnose <= 24.99:
+        case bodyMassIndexDiagnose === 0:
             massIndexMessage = "Your mass index is perfect, keep up the good work while you continue your regular diagnosis";
             break;
 
-        case bodyMassIndexDiagnose < 18.5:
+        case bodyMassIndexDiagnose === -1:
             massIndexMessage = "Increase your body mass to 24.99 - 29.99";
             break;
 
@@ -104,7 +108,7 @@ const diagnoseResult = (cigarettesDiagnose, physicalActivityDiagnose, bodyMassIn
         window.localStorage.setItem("massIndexMessage", massIndexMessage);
         window.localStorage.setItem("bloodPressureMessage", bloodPressureMessage);
         //programmatic redirection to instant-result view
-        window.location.assign("/instant-result");
+        // window.location.assign("/instant-result");
     } else {
         window.alert("Unexpected error encountered");
     }
@@ -127,17 +131,17 @@ const diagnose = () => {
             hours: Number(e.target.hours.value),
             minutes: Number(e.target.minutes.value),
         };
-
         const bloodPressureDiagnose = bloodPressure(_diagnosisFormValue.systolic, _diagnosisFormValue.diastolic);
         const cigarettesDiagnose = cigarettesSmokedPerWeek(_diagnosisFormValue.cigarettesSmoked);
         const physicalActivityDiagnose = physicalActivity(_diagnosisFormValue.hours, _diagnosisFormValue.minutes);
         const bodyMassIndexDiagnose = bodyMassIndex(_diagnosisFormValue.height, _diagnosisFormValue.weight);
 
-        //Stroke level calculation and invocation of the util that adds it to storage for usage in instant-result view
-        const strokeLevel = bloodPressureDiagnose + cigarettesDiagnose + physicalActivityDiagnose + bloodPressureDiagnose / 4;
-        _strokeLevel(strokeLevel);
-
         diagnoseResult(cigarettesDiagnose, physicalActivityDiagnose, bodyMassIndexDiagnose, bloodPressureDiagnose);
+
+        //Stroke level calculation and invocation of the util that adds it to storage for usage in instant-result view
+        const strokeLevelAddition = bloodPressureDiagnose + cigarettesDiagnose + physicalActivityDiagnose + bloodPressureDiagnose;
+        const strokeLevel = strokeLevelAddition / 4;
+        _strokeLevel(strokeLevel);
         e.target.reset();
 
         console.log("BP", bloodPressureDiagnose);
