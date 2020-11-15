a212476494bad00bfd9afa22a65476e615103207
Emmanuel Onah <emmajerry2016@gmail.com>
2020-11-15 07:24:46 +0200

    footer rendering change to js rendering

--- src/components/footer.component.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("DOMContentLoaded", (_) => {
-    const time = document.querySelectorAll(".system--date");
-    ///Update The Date of the System
-    time.forEach((time) => {
-        time.textContent = new Date().getFullYear();
-    });
-});
